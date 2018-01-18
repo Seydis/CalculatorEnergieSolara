eedf9ecc8e3e467055e08759571fb9b3d9a89a66
Ivan David <andreidavid.ivan@gmail.com>
2018-01-18 18:54:17 +0300

    fixes

--- js/contact.js
+++ js/contact.js
@@ -1,45 +1,5 @@
 var url = 'http://localhost:3000/message/';
 
-function getTest() {
-//   fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-//        .then(function (response) {
-//            // Trasform server response to get the dogs
-//            response.json().then(function (dogs) {
-//                log(dogs);
-//            });
-// });
-
-
-fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-.then(
- function(response) {
-   if (response.status !== 200) {
-     console.log('Looks like there was a problem. Status Code: ' +
-       response.status);
-     return;
-   }
-
-   // Examine the text in the response
-   response.json().then(function(data) {
-     console.log(data);
-
-     // var months = document.getElementById('mpc').getElementsByTagName('input');
-     // //console.log(months[1]);
-     // for (var i = 0; i < data.length; i++) {
-     //   var temp = data[i];
-     //   var kWh = temp["kWh"];
-     //   months[i].value = Number(kWh);
-     // }
-   });
- }
-)
-.catch(function(err) {
- console.log('Fetch Error: ', err);
-});
-
-
-}
-
 function postMessage() {
  // console.log(json);
 
